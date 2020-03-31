6d6ccd5a10e760fc2b7035e18e0d4d73bd0dc0bd
STong01 <1461638919@qq.com>
2020-04-01 00:20:59 +0800

    。。。

--- C+.0331/C+.0331/斐波那契凤尾.cpp
+++ C+.0331/C+.0331/斐波那契凤尾.cpp
@@ -2,24 +2,21 @@
 #include <vector>
 using namespace std;
 
-int test(int n)
-{
-	vector<int> tmp = { 1, 1 };
-	int num = 0;
-	for (int i = 2; i <= n; i++)
-	{
-		tmp.push_back(tmp[i - 1] + tmp[i - 2]);
-	}
-	num = tmp[n] % 1000000;
-	return num;
-}
-
 int main()
 {
 	int n;
+	vector<long> tmp = { 1, 1 };
+	for (int i = 2; i <= 100001; i++)
+	{
+		tmp.push_back(0);
+		tmp[i] = ((tmp[i - 1] % 1000000) + (tmp[i - 2] % 1000000)) % 1000000;
+	}
 	while (cin >> n)
 	{
-		cout << test(n) << endl;
+		if (n <= 25)  //加上判断，此时的数列值就是100000的分界处
+			cout << tmp[n] << endl; //直接输出
+		else
+			printf("%06ld\n", tmp[n]);//输出后六位
 	}
 
 	system("pause");
