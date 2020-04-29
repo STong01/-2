319a7efe58a075ebb7433e80564d82a0a361dde9
STong01 <1461638919@qq.com>
2020-04-29 23:36:17 +0800

    实现Singleton模式

---
+++ JZ.2/JZ.2/实现Singleton模式.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+//JAVA
+//public sealed class Singleton1
+//{
+//	private Singleton1()
+//	{}
+//
+//	private static Singleton1 instance = null;
+//	public static Singleton1 Instance
+//	{
+//		get
+//		{
+//		if (instance == null)
+//		{
+//			instance = new Singleton1();
+//		}
+//		return 
+//		}
+//	}
+//};
+
+int main()
+{
+
+	system("pause");
+	return 0;
+}
