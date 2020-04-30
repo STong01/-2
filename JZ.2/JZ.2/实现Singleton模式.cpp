384d106fae38be95c8a051f7a902755fbe0e3f7b
STong01 <1461638919@qq.com>
2020-04-30 22:29:02 +0800

    。。。

--- JZ.2/JZ.2/实现Singleton模式.cpp
+++ JZ.2/JZ.2/实现Singleton模式.cpp
@@ -21,6 +21,33 @@ using namespace std;
 //	}
 //};
 
+//public sealed class Singleton2
+//{
+//	private Singleton2()
+//	{
+//
+//	}
+//
+//	private static readonly object syncObj = new object();
+//
+//	private static Singleton2 instance = null;
+//
+//	public static Singleton2 Instance
+//	{
+//		get
+//		{
+//			lock(syncObj)
+//			{
+//				if (instance == null)
+//				{
+//					instance = new Singleton2();
+//				}
+//			}
+//			return instance;
+//		}
+//	}
+//};
+
 int main()
 {
 
