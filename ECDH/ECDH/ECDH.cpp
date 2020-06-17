5d83e84c8266d8711e8ee1131db808e231a0354d
STong01 <1461638919@qq.com>
2020-06-17 23:40:37 +0800

    ECDH

---
+++ ECDH/ECDH/ECDH.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int Modular_operation(double n, double m)
+{
+	return 0;
+}
+
+vector<int> test(int N, vector<int>Sp0)
+{
+	vector<int>Sp1 = { 0, 0 };
+	if (N == 1)
+	{
+		Sp1[0] = Sp0[0];
+		Sp1[1] = Sp1[1];
+	}
+	if (N == 2)
+	{
+
+	}
+	return Sp1;
+}
+
+int main()
+{
+	double p = 23 / 3;
+	cout << p << endl;
+
+
+	system("pause");
+	return 0;
+}
